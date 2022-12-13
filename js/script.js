72a1552a379186834cd09cf1fad15b052b85f583
Vitaliy <vitaliygosteev@gmail.com>
2022-12-13 21:20:31 +0400

    Implemented mobile navigation functional

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Making mobile navigation work
+const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+
+const mobileNavHandler = () => {
+  headerEl.classList.toggle('nav-open');
+};
+
+btnNavEl.addEventListener('click', mobileNavHandler);
