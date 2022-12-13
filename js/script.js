efc52285c45aefd3aabf773f04430eab04fdab7c
Vitaliy <vitaliygosteev@gmail.com>
2022-12-13 21:24:26 +0400

    Added smooth scrolling animation feature

--- js/script.js
+++ js/script.js
@@ -12,3 +12,31 @@ const mobileNavHandler = () => {
 };
 
 btnNavEl.addEventListener('click', mobileNavHandler);
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((link) =>
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //     Scroll back to the top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    //       Scroll to the other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    //     Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  })
+);
